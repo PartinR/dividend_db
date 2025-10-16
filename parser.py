533569fc19ae91be3f8500ae85ffe5af2ea316ae
Race Partin <racepartin@gmail.com>
2025-10-16 17:13:29 -0400

    Update 'parser.py' to include supabase url and key

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import os
 from supabase import create_client, Client
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("https://egeilyeivfjqylffekal.supabase.co")
+key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZWlseWVpdmZqcXlsZmZla2FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NjA3NDMsImV4cCI6MjA3NjAzNjc0M30.WjeBmbIHE77k7djZbIy_cVpzaj1kQXzYQDtJSxNNHZg")
 supabase: Client = create_client(url, key)
